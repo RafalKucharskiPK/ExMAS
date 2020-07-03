cc6a2f9fbbb80b2306305c670b185ac89af07c9e
Rafal <r.m.kucharski@tudelft.nl>
2020-07-03 14:25:47 +0200

    ExMAS

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='ExMAS',  # How you named your package folder (MyLib)
     packages=['ExMAS'],  # Chose the same as "name"
-    version='0.2',  # Start with a small number and increase it with every change you make
+    version='0.9.9',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="Exact Matching of Attractive Shared rides (ExMAS) Kucharski R, Cats O TransResB, 2020 139 2020 285-310",
     author='Rafal Kucharski',  # Type in your name
     author_email='rkucharski@pk.edu.pl',  # Type in your E-Mail
     url='https://github.com/RafalKucharskiPK/ExMAS',  # Provide either the link to your github or to your website
-    download_url='https://github.com/RafalKucharskiPK/ExMAS/archive/0.0.1.tar.gz',  # I explain this later on
+    download_url='https://github.com/RafalKucharskiPK/ExMAS/archive/0.1.1.tar.gz',  # I explain this later on
     keywords=['MaaS', 'transportation', 'shared mobility'],  # Keywords that define your package best
     install_requires=['networkx>=2.4',
                       'pandas==1.0.5',
