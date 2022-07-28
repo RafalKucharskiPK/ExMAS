ced57609a0b639ae0db5e3b020bb760d16a18477
Michał Bujak <71735125+michallbujak@users.noreply.github.com>
2022-07-28 11:55:40 +0200

    Add files via upload

--- Topology/data_viewer.py
+++ Topology/data_viewer.py
@@ -14,11 +14,11 @@ import netwulf
 # with open('data/results/06-06-22/rep_graphs_06-06-22.obj', 'rb') as file:
 #     e = pickle.load(file)
 
-with open('data/results/06-06-22/dotmap_list_06-06-22.obj', 'rb') as file:
-    e = pickle.load(file)
+# with open('data/results/06-06-22/dotmap_list_06-06-22.obj', 'rb') as file:
+#     e = pickle.load(file)
 
-# with open('data/results/06-06-22/all_graphs_list_06-06-22.obj', 'rb') as file:
-#     e = pickle.load(file)[0]
+with open('data/results/06-06-22/all_graphs_list_06-06-22.obj', 'rb') as file:
+    e = pickle.load(file)[0]
 
 topological_config = utils.get_parameters('data/configs/topology_settings.json')
 # utils.create_results_directory(topological_config)
@@ -51,20 +51,20 @@ topological_config.path_results = 'data/results/06-06-22/'
 
 
 # visualize(e['pairs_matching'], config=json.load(open('data/configs/netwulf_config.json')))
-# draw_bipartite_graph(e['bipartite_shareability'], 1, topological_config, date='06-06-22', save=True,
-#                      name="bipartite_shareability_single", dpi=200, colour_specific_node=0)
+draw_bipartite_graph(e['bipartite_matching'], 1, topological_config, date='06-06-22', save=True,
+                     name="bipartite_matching_single", dpi=200, colour_specific_node=0)
 
-num_list = [1, 5, 10, 100, 900]
-for num in num_list:
-    if num == 1:
-        obj = [e[1]]
-    else:
-        obj = e[:num]
-    draw_bipartite_graph(utils.analyse_edge_count(obj, topological_config,
-                                                  list_types_of_graph=['bipartite_matching'], logger_level='WARNING')[
-                             'bipartite_matching'],
-                         num, node_size=1, dpi=80, figsize=(10, 24), plot=False, width_power=1,
-                         config=topological_config, save=True, saving_number=num, date='06-06-22')
+# num_list = [1, 5, 10, 100, 900]
+# for num in num_list:
+#     if num == 1:
+#         obj = [e[1]]
+#     else:
+#         obj = e[:num]
+#     draw_bipartite_graph(utils.analyse_edge_count(obj, topological_config,
+#                                                   list_types_of_graph=['bipartite_matching'], logger_level='WARNING')[
+#                              'bipartite_matching'],
+#                          num, node_size=1, dpi=80, figsize=(10, 24), plot=False, width_power=1,
+#                          config=topological_config, save=True, saving_number=num, date='06-06-22')
 
 # fig, axes = plt.subplots(nrows=2, ncols=5, sharex='col', sharey='row')
 #
